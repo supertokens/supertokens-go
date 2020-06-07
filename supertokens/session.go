05315ad9022580e3ba59640c3f570fc7435fbf81
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-06-07 16:44:10 +0530

    completes session struct functions

--- supertokens/session.go
+++ supertokens/session.go
@@ -1,6 +1,11 @@
 package supertokens
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/supertokens/supertokens-go/supertokens/core"
+	"github.com/supertokens/supertokens-go/supertokens/errors"
+)
 
 // Session object returned for managing a session
 type Session struct {
@@ -13,19 +18,70 @@ type Session struct {
 
 // RevokeSession function used to revoke a session for this session
 func (session *Session) RevokeSession() error {
-	// TODO:
+	success, err := RevokeSession(session.sessionHandle)
+	if err != nil {
+		return err
+	}
+	if success {
+		handShakeInfo, handShakeInfoErr := core.GetHandshakeInfoInstance()
+		if handShakeInfoErr != nil {
+			return handShakeInfoErr
+		}
+		clearSessionFromCookie(session.response,
+			handShakeInfo.CookieDomain,
+			handShakeInfo.CookieSecure,
+			handShakeInfo.AccessTokenPath,
+			handShakeInfo.RefreshTokenPath,
+			handShakeInfo.IDRefreshTokenPath,
+			handShakeInfo.CookieSameSite,
+		)
+	}
 	return nil
 }
 
 // GetSessionData function used to get session data for this session
 func (session *Session) GetSessionData() (map[string]interface{}, error) {
-	// TODO:
-	return map[string]interface{}{}, nil
+	data, err := GetSessionData(session.sessionHandle)
+	if err != nil {
+		if errors.IsUnauthorisedError(err) {
+			handShakeInfo, handShakeInfoErr := core.GetHandshakeInfoInstance()
+			if handShakeInfoErr != nil {
+				return nil, handShakeInfoErr
+			}
+			clearSessionFromCookie(session.response,
+				handShakeInfo.CookieDomain,
+				handShakeInfo.CookieSecure,
+				handShakeInfo.AccessTokenPath,
+				handShakeInfo.RefreshTokenPath,
+				handShakeInfo.IDRefreshTokenPath,
+				handShakeInfo.CookieSameSite,
+			)
+		}
+		return nil, err
+	}
+	return data, nil
 }
 
 // UpdateSessionData function used to update session data for this session
 func (session *Session) UpdateSessionData(newSessionData map[string]interface{}) error {
-	// TODO:
+	err := UpdateSessionData(session.sessionHandle, newSessionData)
+	if err != nil {
+		if errors.IsUnauthorisedError(err) {
+			handShakeInfo, handShakeInfoErr := core.GetHandshakeInfoInstance()
+			if handShakeInfoErr != nil {
+				return handShakeInfoErr
+			}
+			clearSessionFromCookie(session.response,
+				handShakeInfo.CookieDomain,
+				handShakeInfo.CookieSecure,
+				handShakeInfo.AccessTokenPath,
+				handShakeInfo.RefreshTokenPath,
+				handShakeInfo.IDRefreshTokenPath,
+				handShakeInfo.CookieSameSite,
+			)
+		}
+		return err
+	}
 	return nil
 }
 
@@ -51,6 +107,36 @@ func (session *Session) GetAccessToken() string {
 
 // UpdateJWTPayload function used to update jwt payload for this session
 func (session *Session) UpdateJWTPayload(newJWTPayload map[string]interface{}) error {
-	// TODO:
+	sessionInfo, err := core.RegenerateSession(session.accessToken, newJWTPayload)
+	if err != nil {
+		if errors.IsUnauthorisedError(err) {
+			handShakeInfo, handShakeInfoErr := core.GetHandshakeInfoInstance()
+			if handShakeInfoErr != nil {
+				return handShakeInfoErr
+			}
+			clearSessionFromCookie(session.response,
+				handShakeInfo.CookieDomain,
+				handShakeInfo.CookieSecure,
+				handShakeInfo.AccessTokenPath,
+				handShakeInfo.RefreshTokenPath,
+				handShakeInfo.IDRefreshTokenPath,
+				handShakeInfo.CookieSameSite,
+			)
+		}
+		return err
+	}
+	session.userDataInJWT = sessionInfo.UserDataInJWT
+	if sessionInfo.AccessToken != nil {
+		session.accessToken = (*sessionInfo.AccessToken).Token
+		attachAccessTokenToCookie(
+			session.response,
+			(*sessionInfo.AccessToken).Token,
+			(*sessionInfo.AccessToken).Expiry,
+			(*sessionInfo.AccessToken).Domain,
+			(*sessionInfo.AccessToken).CookieSecure,
+			(*sessionInfo.AccessToken).CookiePath,
+			(*sessionInfo.AccessToken).SameSite,
+		)
+	}
 	return nil
 }
